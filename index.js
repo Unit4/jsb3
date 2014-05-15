968fcff259046a56b01fec6b63fbf6e832cb111f
Bjørn Harald Olsen <bjornhol@gmail.com>
2014-05-15 10:03:05 +0200

    Add source and tests

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var Q = require('q');
+var JSONStream = require('JSONStream');
+
+module.exports = function(jsb3){
+	var deferred = Q.defer(),
+        input = fs.createReadStream(jsb3),
+        files = [];
+
+    input.pipe(JSONStream.parse(['packages', true, 'files']))
+        .on('data', function(data) {
+            data.forEach(function(file) {
+                files.push(path.join(path.dirname(jsb3), file.path, file.name));
+            });
+        })
+        .on('end', function() {
+            deferred.resolve(files);
+        })
+        .on('error', function(error) {
+            deferred.reject(error);
+        });
+
+    return deferred.promise;
+};
---
+++ test.js
@@ -0,0 +1,33 @@
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var jsb3 = require('./index');
+
+it('should read all files from jsb3 stream', function(cb){
+	jsb3('testfiles/file1.jsb3').then(function(files){
+		assert.equal(files.length, 2);
+		assert.strictEqual('folder1', path.dirname(files[0]));		
+		assert.strictEqual('file.txt', path.basename(files[0]));
+		assert.strictEqual('folder2', path.dirname(files[1]));
+		assert.strictEqual('file.pdf', path.basename(files[1]));		
+		cb();
+	}).fail(function(message){
+		assert(false, message);
+		cb();
+	});	
+});
+
+it('should read all files from jsb3 stream with multiple projects', function(cb){
+	jsb3('testfiles/file2.jsb3').then(function(files){
+		assert.equal(files.length, 2);
+		assert.strictEqual('folder1', path.dirname(files[0]));		
+		assert.strictEqual('file.txt', path.basename(files[0]));
+		assert.strictEqual('folder2', path.dirname(files[1]));
+		assert.strictEqual('file.pdf', path.basename(files[1]));		
+		cb();
+	}).fail(function(message){
+		assert(false, message);
+		cb();
+	});	
+	cb();
+});
